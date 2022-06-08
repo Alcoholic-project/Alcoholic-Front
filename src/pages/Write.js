dc732d16a2d19626342d35f1102e528dd71e9f02
KimGaeun0806 <greencloud11@hanyang.ac.kr>
2022-06-08 11:28:00 +0900

    Feat: Create adding input func

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,7 +1,7 @@
 import Header from '../components/Header';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 // 1. 레시피 이름
 // 2. 술 종류
@@ -25,6 +25,8 @@ const DrinkAmountInput = styled.input``;
 
 const DrinkPlusBtn = styled.button``;
 
+let num = 0;
+
 const Write = () => {
   const [input, setInput] = useState({
     recipeName: '',
@@ -33,25 +35,39 @@ const Write = () => {
     photo: '',
   });
 
-  const [drink, setDrink] = useState([]);
+  const [drinkPlus, setDrinkPlus] = useState([0]);
+  const [drinkName, setDrinkName] = useState({});
+  const [drinkAmount, setDrinkAmount] = useState({});
 
-  const onClickDrinkPlus = () => {};
+  const onClickDrinkPlus = () => {
+    if (num < 4) {
+      num++;
+      setDrinkPlus([...drinkPlus, num]);
+    }
+  };
+
+  const onChangeDrinkName = (e) => {
+    setDrinkName({ ...drinkName, [e.target.name]: e.target.value });
+  };
+
+  const onChangeDrinkAmount = (e) => {
+    setDrinkAmount({ ...drinkAmount, [e.target.name]: e.target.value });
+  };
 
   return (
     <>
       <Header />
       <Container>
         <RecipeNameInput />
-        {drink.map((num) => (
-          <DrinkBox key={num}>
-            <DrinkNameInput />
-            <DrinkAmountInput />
+        {drinkPlus.map((n) => (
+          <DrinkBox key={n}>
+            <DrinkNameInput name={n} onChange={onChangeDrinkName} />
+            <DrinkAmountInput name={n} onChange={onChangeDrinkAmount} />
           </DrinkBox>
         ))}
 
         <DrinkPlusBtn onClick={onClickDrinkPlus}>추가</DrinkPlusBtn>
         {/* 동그라미 플러스 버튼으로 넣기  */}
-        {/* 빼기 버튼도 넣기  */}
       </Container>
     </>
   );
